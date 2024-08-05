0b7c174afd53a2843fe9bfccc406eb717c61d4c6
klausmonga <monga.wakowa@gmail.com>
2024-08-05 14:45:27 +0200

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import os
+import json
+import random
+import multiprocessing
+import time
+
+def Onstart():
+    b = 0
+    while 1:
+        # print(b)
+        time.sleep(1)
+        random.random()
+        b = random.randint(0, 50)
+        os.system("/usr/bin/mosquitto_pub -h 127.0.0.1 -t ch1/temp -m " + str(b))
+
